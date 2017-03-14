86218978f390f7ccd322fd8482dd6a990f16e113
elevenBeans <elevenbeansf2e@gmail.com>
2017-03-14 20:17:00 +0800

    need function called valueStrToResult

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,59 +15,48 @@ const App = () => (
 
 const Title = () => (
 	<div id="title" className = "title text-center">
-		<h4>
 			<b>CALCULATOR</b>
-		</h4>
 	</div>
-)
-
-const CopyRight = () => (
-	<div className = "copy-right">
-	Designed & Coded by ElevenBeans
-	</div>
-)
+);
 
 var Content = React.createClass({
 
+	getInitialState: function(){
+		return{
+			valueStr: '',
+			result: '0'
+		}
+	},
+	valueStrToResult: function(str){
+
+	},
+	getResult: function(value){
+		console.log('asdf:',value);
+		
+		var _valueStr = this.state.valueStr;
+		
+		if(value == '='){
+			this.setState({
+				valueStr: '',
+				result: _valueStr.replace(/\+/ig,'')
+			});
+			return false;
+		} else if (value == 'ac'){
+			this.setState({result: 0});
+			return false;
+		}
+		
+		this.setState({
+			valueStr: _valueStr + value,
+			result: _valueStr + value
+		});
+
+	},
 	render: function(){
 		return (
 			<div>
-				<Display />
-				<Buttons />
-			</div>
-		)
-	}
-});
-
-var Buttons = React.createClass({
-	
-	render: function(){
-		return (
-			<div id="buttons">
-	      <button className="red" value="ac">AC</button>
-	      <button className="red" value="ce">CE</button>
-	      <button value="/">÷</button>
-	      <button value="*">x</button>
-
-	      <button className="white" value="7">7</button>
-	      <button className="white" value="8">8</button>
-	      <button className="white" value="9">9</button>
-	      <button value="-">-</button>
-
-	      <button className="white" value="4">4</button>
-	      <button className="white" value="5">5</button>
-	      <button className="white" value="6">6</button>
-	      <button value="+">+</button>
-
-
-	      <button className="white" value="1">1</button>
-	      <button className="white" value="2">2</button>
-	      <button className="white" value="3">3</button>
-	      <button className="invisible">N</button>
-
-	      <button id="zeroButton" value="0">0</button>
-	      <button value=".">.</button>
-	      <button className="equalButton red" id="equalButton" value="=">=</button>
+				<Display result = {this.state.result}/>
+				<Buttons setValue = {this.getResult}/>
 			</div>
 		)
 	}
@@ -79,13 +68,54 @@ var Display = React.createClass({
 		return (
 			<div id="entrybox" className="text-right">
 				<div id="entry">
-					<p id="answer">0</p>
+					<p id="answer"> {this.props.result} </p>
 				</div>
 			</div>
 		)
 	}
 });
 
+var Buttons = React.createClass({
+
+	clickHandler: function(e){
+		this.props.setValue(e.target.value);
+	},
+	render: function(){
+		return (
+			<div id="buttons">
+	      <button onClick={this.clickHandler} className="red" value="ac">AC</button>
+	      <button onClick={this.clickHandler} value="/">÷</button>
+	      <button onClick={this.clickHandler} value="*">x</button>
+	      <button onClick={this.clickHandler} value="-">-</button>
+
+	      <button onClick={this.clickHandler} className="white" value="8">8</button>
+	      <button onClick={this.clickHandler} className="white" value="9">9</button>
+	      <button onClick={this.clickHandler} className="white" value="7">7</button>
+	      <button onClick={this.clickHandler} value="+">+</button>
+
+	      <button onClick={this.clickHandler} className="white" value="5">5</button>
+	      <button onClick={this.clickHandler} className="white" value="6">6</button>
+	      <button onClick={this.clickHandler} className="white" value="4">4</button>
+
+	      <button onClick={this.clickHandler} className="invisible">N</button>
+
+	      <button onClick={this.clickHandler} className="white" value="2">2</button>
+	      <button onClick={this.clickHandler} className="white" value="3">3</button>
+	      <button onClick={this.clickHandler} className="white" value="1">1</button>
+
+	      <button onClick={this.clickHandler} id="zeroButton" value="0">0</button>
+	      <button onClick={this.clickHandler} value=".">.</button>
+	      <button onClick={this.clickHandler} className="equalButton red" id="equalButton" value="=">=</button>
+			</div>
+		)
+	}
+});
+
+const CopyRight = () => (
+	<div className = "copy-right">
+		Designed & Coded by ElevenBeans
+	</div>
+);
 
 // 最终渲染
 ReactDom.render((
